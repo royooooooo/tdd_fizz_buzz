a2586bc5c4ce182653f108cec224cde951e96033
Fu Yi <yifu@thoughtworks.com>
2020-01-10 23:40:32 +0800

    refactor code by use temp array.

--- src/main/java/com.thoughtworks/FizzBuzzUtil.java
+++ src/main/java/com.thoughtworks/FizzBuzzUtil.java
@@ -1,6 +1,7 @@
 package com.thoughtworks;
 
 import static java.lang.String.valueOf;
+import static java.util.Objects.isNull;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -19,20 +20,24 @@ public class FizzBuzzUtil {
 
   public static String fizzBuzz(int number) {
     String correctSlogan = "";
+    int[] specificMultipleNumbers = null;
     if (isContainsCharacter(number, 7)) {
       if (isContainsCharacter(number, 3)) {
         return specificNumberAndSlogan.get(3);
       }
-      correctSlogan += getSloganByMultipleOfSpecificNumbers(number, new int[] {3, 7});
-      return correctSlogan;
-    }else if (isContainsCharacter(number, 5)) {
+      specificMultipleNumbers = new int[] {3, 7};
+    } else if (isContainsCharacter(number, 5)) {
       correctSlogan += specificNumberAndSlogan.get(5);
-      correctSlogan += getSloganByMultipleOfSpecificNumbers(number, new int[] {7});
-      return correctSlogan;
-    }else if (isContainsCharacter(number, 3)) {
+      specificMultipleNumbers = new int[] {7};
+    } else if (isContainsCharacter(number, 3)) {
       return specificNumberAndSlogan.get(3);
     }
-    correctSlogan += getSloganByMultipleOfSpecificNumbers(number, new int[] {3, 5, 7});
+
+    if (isNull(specificMultipleNumbers)) {
+      specificMultipleNumbers = new int[] {3, 5, 7};
+    }
+
+    correctSlogan += getSloganByMultipleOfSpecificNumbers(number, specificMultipleNumbers);
     if (correctSlogan.equals("")) correctSlogan += number;
     return correctSlogan;
   }
