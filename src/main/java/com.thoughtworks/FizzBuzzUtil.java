ba2fdadd8121300df3a5e825beba9522068e1441
Fu Yi <yifu@thoughtworks.com>
2020-01-10 22:16:18 +0800

    remove hard code.

--- src/main/java/com.thoughtworks/FizzBuzzUtil.java
+++ src/main/java/com.thoughtworks/FizzBuzzUtil.java
@@ -6,8 +6,6 @@ public class FizzBuzzUtil {
 
   public static String fizzBuzz(int number) {
     String correctSlogan = "";
-    if (number == 1) correctSlogan += "1";
-    if (number == 2) correctSlogan += "2";
     if (isContainsCharacter(number, "7")) {
       if (isContainsCharacter(number, "3")) {
         return "Fizz";
@@ -25,6 +23,7 @@ public class FizzBuzzUtil {
     if (number % 3 == 0) correctSlogan += "Fizz";
     if (number % 5 == 0) correctSlogan += "Buzz";
     if (number % 7 == 0) correctSlogan += "Whizz";
+    if (correctSlogan.equals("")) correctSlogan += number;
     return correctSlogan;
   }
 
