00d66f462f779c0d69f5bdc99cbde26eb7876191
Fu Yi <yifu@thoughtworks.com>
2020-01-10 23:24:35 +0800

    use map to replace the judge.

--- src/main/java/com.thoughtworks/FizzBuzzUtil.java
+++ src/main/java/com.thoughtworks/FizzBuzzUtil.java
@@ -2,32 +2,51 @@ package com.thoughtworks;
 
 import static java.lang.String.valueOf;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FizzBuzzUtil {
+
+  public static Map<Integer, String> specificNumberAndSlogan;
+
+  static {
+    specificNumberAndSlogan = new HashMap<>();
+    specificNumberAndSlogan.put(3, "Fizz");
+    specificNumberAndSlogan.put(5, "Buzz");
+    specificNumberAndSlogan.put(7, "Whizz");
+  }
 
   public static String fizzBuzz(int number) {
     String correctSlogan = "";
-    if (isContainsCharacter(number, "7")) {
-      if (isContainsCharacter(number, "3")) {
-        return "Fizz";
+    if (isContainsCharacter(number, 7)) {
+      if (isContainsCharacter(number, 3)) {
+        return specificNumberAndSlogan.get(3);
       }
-      if (number % 3 == 0) correctSlogan += "Fizz";
-      if (number % 7 == 0) correctSlogan += "Whizz";
+      correctSlogan += getSloganByMultipleOfSpecificNumbers(number, new int[] {3, 7});
       return correctSlogan;
-    } else if (isContainsCharacter(number, "5")) {
-      correctSlogan += "Buzz";
-      if (number % 7 == 0) correctSlogan += "Whizz";
+    }else if (isContainsCharacter(number, 5)) {
+      correctSlogan += specificNumberAndSlogan.get(5);
+      correctSlogan += getSloganByMultipleOfSpecificNumbers(number, new int[] {7});
       return correctSlogan;
-    } else if (isContainsCharacter(number, "3")) {
-      return "Fizz";
+    }else if (isContainsCharacter(number, 3)) {
+      return specificNumberAndSlogan.get(3);
     }
-    if (number % 3 == 0) correctSlogan += "Fizz";
-    if (number % 5 == 0) correctSlogan += "Buzz";
-    if (number % 7 == 0) correctSlogan += "Whizz";
+    correctSlogan += getSloganByMultipleOfSpecificNumbers(number, new int[] {3, 5, 7});
     if (correctSlogan.equals("")) correctSlogan += number;
     return correctSlogan;
   }
 
-  private static boolean isContainsCharacter(int number, String s) {
-    return valueOf(number).contains(s);
+  private static String getSloganByMultipleOfSpecificNumbers(int number, int[] specificNumbers) {
+    return Arrays.stream(specificNumbers)
+        .boxed()
+        .map(
+            specificNumber ->
+                number % specificNumber == 0 ? specificNumberAndSlogan.get(specificNumber) : "")
+        .reduce("", String::concat);
+  }
+
+  private static boolean isContainsCharacter(int number, int specificNumber) {
+    return valueOf(number).contains(valueOf(specificNumber));
   }
 }
