249450e0c527d513cd90f3ccdaa4e99a05c29eac
Roy <292701927@qq.com>
2020-01-11 23:20:12 +0800

    use common method to replace hard code.

--- src/main/java/com.thoughtworks/FizzBuzzUtil.java
+++ src/main/java/com.thoughtworks/FizzBuzzUtil.java
@@ -1,34 +1,1 @@
-package com.thoughtworks;
-
-import static java.lang.String.valueOf;
-
-public class FizzBuzzUtil {
-
-  public static String fizzBuzz(int number) {
-    String correctSlogan = "";
-    if (number == 1) correctSlogan += "1";
-    if (number == 2) correctSlogan += "2";
-    if (isContainsCharacter(number, "7")) {
-      if (isContainsCharacter(number, "3")) {
-        return "Fizz";
-      }
-      if (number % 3 == 0) correctSlogan += "Fizz";
-      if (number % 7 == 0) correctSlogan += "Whizz";
-      return correctSlogan;
-    } else if (isContainsCharacter(number, "5")) {
-      correctSlogan += "Buzz";
-      if (number % 7 == 0) correctSlogan += "Whizz";
-      return correctSlogan;
-    } else if (isContainsCharacter(number, "3")) {
-      return "Fizz";
-    }
-    if (number % 3 == 0) correctSlogan += "Fizz";
-    if (number % 5 == 0) correctSlogan += "Buzz";
-    if (number % 7 == 0) correctSlogan += "Whizz";
-    return correctSlogan;
-  }
-
-  private static boolean isContainsCharacter(int number, String s) {
-    return valueOf(number).contains(s);
-  }
-}
+package com.thoughtworks;import static java.lang.String.valueOf;class FizzBuzzUtil {  static String fizzBuzz(int number) {    String correctSlogan = "";    if (isContainsCharacter(number, "7")) {      if (isContainsCharacter(number, "3")) {        return "Fizz";      }      if (number % 3 == 0) correctSlogan += "Fizz";      if (number % 7 == 0) correctSlogan += "Whizz";      return correctSlogan;    } else if (isContainsCharacter(number, "5")) {      correctSlogan += "Buzz";      if (number % 7 == 0) correctSlogan += "Whizz";      return correctSlogan;    } else if (isContainsCharacter(number, "3")) {      return "Fizz";    }    if (number % 3 == 0) correctSlogan += "Fizz";    if (number % 5 == 0) correctSlogan += "Buzz";    if (number % 7 == 0) correctSlogan += "Whizz";    if (correctSlogan.equals("")) correctSlogan += number;    return correctSlogan;  }  private static boolean isContainsCharacter(int number, String s) {    return valueOf(number).contains(s);  }}
